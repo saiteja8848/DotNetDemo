c69b19c12bedfc70726be8bd007929fb07cb9e45
saiteja8848 <saiteja8848@gmail.com>
2021-02-12 14:01:02 +0530

    firstcommit

---
+++ DotNetDemo.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetDemo.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void addTestMethod()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(2, calculator.add(1, 1));
+        }
+    }
+}
---
+++ DotNetDemo/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetDemo
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+        }
+    }
+    public class Calculator
+    {
+
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    
+}
